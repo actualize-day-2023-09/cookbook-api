8fe7323a10b6650dfbc2014b5c9dbcecb24ce58a
amanda <amanda@actualize.co>
2023-10-23 12:00:44 -0500

    uses rails generator to create model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,11 +1,2 @@
-class Recipe
-  attr_accessor :title, :ingredients, :directions, :prep_time, :chef
-
-  def initialize(options)
-    @title = options[:title]
-    @ingredients = options[:ingredients]
-    @directions = options[:directions]
-    @prep_time = options[:prep_time]
-    @chef = options[:chef]
-  end
+class Recipe < ApplicationRecord
 end
