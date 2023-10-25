1420dd86daca658253250fbffb3fbd2ad34ddf0a
amanda <amanda@actualize.co>
2023-10-25 12:45:29 -0500

    adds create method with hard coded data

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,4 +8,17 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find_by(id: params["id"])
     render :show
   end
+
+  def create
+    @recipe = Recipe.new(
+      title: "Mud Pie",
+      chef: "Peter Jang",
+      image_url: "image.png",
+      prep_time: 10,
+      ingredients: "mud",
+      directions: "bake the mud. Enjoy!",
+    )
+    @recipe.save
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  get "/recipes" => "recipes#index" #RESTful conventions
-  # get "/one_recipe", controller: "recipes", action: "one_recipe"
+  post "/recipes" => "recipes#create"
+  get "/recipes" => "recipes#index"
   get "/recipes/:id" => "recipes#show"
 end
